016ea0083e53e9bc641be9586be7637d7f16fe2f
Lebedev Sergey <oddyone@mail.ru>
2017-07-05 14:12:12 +0300

    Доделал футер

--- src/js/script.js
+++ src/js/script.js
@@ -51,6 +51,25 @@ $(document).ready(function(){
     // instead of a settings object
   ]
   });
+  $('.carousel').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+  });
 // $('.header__nav-xs').slicknav({
 //   });
 });
